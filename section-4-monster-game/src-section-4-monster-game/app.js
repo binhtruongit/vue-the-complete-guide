05180dd78b2f63492eb930e81910915e80ca0628
Binh Truong An <binhta@irtech.com.vn>
2023-08-18 22:21:47 +0700

    Finish dev attack button

--- section-4-monster-game/src-section-4-monster-game/app.js
+++ section-4-monster-game/src-section-4-monster-game/app.js
@@ -1,23 +1,32 @@
 function getRandomValue(min, max) {
-    return Math.floor(random() * (max - min)) + min;
+    return Math.floor(Math.random() * (max - min)) + min;
+    debugger
 }
 
 const app = Vue.createApp({
     data() {
         return {
-            playerHealth: 100,
             monsterHealth: 100,
+            playerHealth: 100
         };
+    },
+    computed:{
+        monsterBarStyles(){
+            return {width: this.monsterHealth + '%'}
+        },
+        playerBarStyles(){
+            return {width: this.playerHealth + '%'}
+        }
     },
     methods: {
         attackMonster() {            
             const attackValue = getRandomValue(12,5);
-            this.monsterHealth -= -attackValue;
-            this.attackPlayer;
+            this.monsterHealth -= attackValue;
+            this.attackPlayer();
         },
         attackPlayer() {            
             const attackValue = getRandomValue(15,8);
-            this.playerHealth -= -attackValue;
+            this.playerHealth -= attackValue;
         },
     },
 });
