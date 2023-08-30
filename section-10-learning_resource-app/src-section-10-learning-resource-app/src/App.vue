e2dafc4497df7b3f7aed892f82a2308099539da0
Binh Truong An <binhta@irtech.com.vn>
2023-08-30 16:46:31 +0700

    Finish dev basic component

---
+++ section-10-learning_resource-app/src-section-10-learning-resource-app/src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <ul>
+    <learning-resource
+      v-for="res in storageResources"
+      :key="res.id"
+      :title="res.title"
+      :description="res.description"
+      :link="res.link"
+    ></learning-resource>
+  </ul>
+</template>
+
+<script>
+import LearningResource from './components/learning-resource/LearningResource.vue';
+
+export default {
+  components: {
+    LearningResource,
+  },
+  data() {
+    return {
+      storageResources: [
+        { id: '1', title: 'Bitcoin', description: 'Bitcoin Fundamental', link: 'https://bitcoin.com' },
+        { id: '1', title: 'Ethereum', description: 'Ethereum in  Advanced', link: 'https://bitcoin.com' }
+      ],
+    };
+  },
+};
+</script>
